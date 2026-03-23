74897a1ee494ab752f85c20bd6d774aacfdf7142
Mokshagna <mokshagnaappini@gmail.com>
2026-03-23 18:31:52 +0530

    Fixed identation error in db connection

--- utils/db.py
+++ utils/db.py
@@ -1,14 +1,14 @@
 import mysql.connector
-from config import Config
 
 def get_db_connection():
-try:
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="agribed"
-    )
-except:
-    print("DB not connected")
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="agribed"
+        )
+        return connection
+    except Exception as e:
+        print("Database connection error:", e)
         return None
